2819dc43c3fb74ee3393cfd53ccbe7bd5c635dd0
Tan Jing Han <98072503+TanJingHan@users.noreply.github.com>
2023-09-25 00:37:28 +0800

    Update connection.php

--- iMIS-master/connection.php
+++ iMIS-master/connection.php
@@ -1,10 +1,3 @@
 <?php
-// Replace with your AWS MariaDB credentials
-$host = '34.201.142.83'; // AWS MariaDB public IPv4 address
-$username = 'admin'; // Your MariaDB username
-$password = 'lab-password'; // Your MariaDB password
-$database = 'imis'; // Your MariaDB database name
-
-// Create a connection to the AWS MariaDB database
-$con = mysqli_connect($host, $username, $password, $database);
+$con=mysqli_connect('34.201.142.83',  'root', '','imis');
 ?>
