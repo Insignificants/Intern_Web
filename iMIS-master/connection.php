5e14a7eeb535b6371d6dc558ed9b629e6b1ead37
Tan Jing Han <98072503+TanJingHan@users.noreply.github.com>
2023-09-25 12:26:38 +0800

    Update connection.php

--- iMIS-master/connection.php
+++ iMIS-master/connection.php
@@ -1,3 +1,9 @@
 <?php
-$con=mysql_connect('inventory-db.ci8vjilkvded.us-east-1.rds.amazonaws.com',  'admin', 'lab-password', 'cloudDB');
+$host = 'inventory-db.ci8vjilkvded.us-east-1.rds.amazonaws.com'; // replace with your endpoint
+$db   = 'cloudDB'; // replace with your database name
+$user = 'admin'; // replace with your username
+$pass = 'lab-password'; // replace with your password
+
+$dsn = "mysql:host=$host;dbname=$db";
+$pdo = new PDO($dsn, $user, $pass);
 ?>
