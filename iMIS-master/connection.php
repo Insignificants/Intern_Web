c81f25e65e1135621089b08e1b5f5991dd729927
Tan Jing Han <98072503+TanJingHan@users.noreply.github.com>
2023-09-25 12:20:20 +0800

    Update connection.php

--- iMIS-master/connection.php
+++ iMIS-master/connection.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysql_connect('inventory-db.ci8vjilkvded.us-east-1.rds.amazonaws.com',  'admin', 'lab-password', 'imis');
+$con=mysql_connect('inventory-db.ci8vjilkvded.us-east-1.rds.amazonaws.com',  'admin', 'lab-password', 'cloudDB');
 ?>
